0a407c1fcf52f4ec7239c6fc33612a2c010bfb58
feran <therealyoung247@gmail.com>
2025-09-30 18:49:08 +0100

    done?

--- xarm1/xarm/settings.py
+++ xarm1/xarm/settings.py
@@ -90,6 +90,8 @@ USE_TZ = True
 # Static files
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "xarm/templates")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "xarm/static")]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
